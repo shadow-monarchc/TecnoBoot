29b36ae124dc8e2337f836a085f4ea8357691944
juan manuel <juanpersonalf@gmail.com>
2024-11-10 07:32:55 -0400

    Add files via upload

---
+++ javascript/apps.js
@@ -0,0 +1,19 @@
+function pregunta1(){
+    window.location.href = "./html/indexpreguntas.htm#dato-1"
+}
+function pregunta2(){
+    window.location.href = "./html/indexpreguntas.htm#dato-2"
+}
+function pregunta3(){
+    window.location.href = "./html/indexpreguntas.htm#dato-3"
+}
+function pregunta4(){                                                             
+    window.location.href = "./html/indexpreguntas.htm#dato-4"
+}
+function pregunta5(){
+    window.location.href = "./html/indexpreguntas.htm#dato-5"
+    
+}
+function pregunta6(){
+    window.location.href = "./html/indexpreguntas.htm#dato-5"
+}
